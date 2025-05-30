9f43c978e10713f26efc26fae351cad786d1aa12
Oyindamola J. Lewu <oyindamola.lewu@pau.edu.ng>
2025-05-30 12:04:32 +0100

    page structure and placeholder media

---
+++ elirabysoniru/app/components/navbar.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next';
+import Image from "next/image";
+import { useState } from 'react';
+
+const Navbar: NextPage = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="w-full sticky relative bg-background overflow-hidden flex flex-col items-center justify-start lg:pt-5 px-5 lg:px-20 pb-5 box-border gap-[30px] text-center text-2xl text-maroon">
+      
+      {/* Logo and Menu Toggle */}
+      <div className="self-stretch flex flex-row items-center justify-between gap-0 font-3">
+        <Image className="w-[39px] relative max-h-full object-cover" width={39} height={54} sizes="100vw" alt="" src="/logo.png" />
+        
+        <div className="w-[119px] rounded-xl flex flex-col items-center justify-center py-[5px] px-0 box-border">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="w-[119px] rounded-xl flex items-center justify-center py-[5px] px-0 text-2xl font-glacial-indifference"
+          >
+            <div>{menuOpen ? '↓ MENU' : '→ MENU'}</div>
+          </button>
+        </div>
+      </div>
+
+      {/* Conditional Menu Links */}
+      {menuOpen && (
+  <div className="w-full grid grid-cols-1 grid-rows-7 md:grid-cols-3 md:grid-rows-3 lg:grid-cols-4 lg:grid-rows-2 gap-x-8 gap-y-4 font-2 text-left">
+    <div>Home</div>
+    <div>About Us</div>
+    <div>Portfolio</div>
+    <div>The Process</div>
+    <div>Services</div>
+    <div>Testimonials</div>
+    <div>Contact Us</div>
+    <div></div>
+  </div>
+)}
+    </div>
+  );
+};
+
+export default Navbar;
--- elirabysoniru/app/page.tsx
+++ elirabysoniru/app/page.tsx
@@ -1,103 +1,184 @@
+"use client";
+import Navbar from "./components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="bg-background">
+      <main>
+        <Navbar />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        {/* HERO */}
+        <section
+          id="hero"
+          className="w-full relative bg-background flex flex-col items-center justify-center md:pt-5 pb-10 md:px-20  box-border"
+        >
+          <div className="self-stretch md:rounded-[20px] h-full min-h-[700px] bg-[url('/herobg.jpg')] bg-cover bg-no-repeat bg-[top]">
+            <div className="self-stretch rounded-[20px] h-full min-h-[700px] flex flex-col items-start justify-start bg-black/50">
+              {/* Heading */}
+              <div className="w-full h-1/2 text-start px-10 lg:px-20 pt-10">
+                <h1 className="font-1 text-white text-[100px] md:text-[140px] lg:text-[180px]">
+                  ELIRA BY SONIRU.
+                </h1>
+              </div>
+              {/* Content Row */}
+              <div className="w-full h-1/2 flex flex-col md:flex-row items-start align-center  px-10 lg:px-20 pt-10 gap-20">
+                {/* Text Block */}
+                <div className="w-full md:w-1/2 h-full text-white font-3 flex flex-col items-start justify-center gap-5">
+                  <p className="text-2xl md:text-4xl w-4/5">
+                    Decluttering & interior styling for busy professionals.
+                  </p>
+                  <div className="rounded-md bg-gray-100/10 border-linen border-solid border-[0.5px] py-3 px-4">
+                    <div className="text-sm md:text-base">
+                      Book Your Consultation →
+                    </div>
+                  </div>
+                </div>
+                {/* Image Block */}
+                <div className="hidden w-full md:w-1/2 h-full md:flex items-center justify-center p-6">
+                  <Image
+                    className="w-[180px] sm:w-[220px] md:w-[260px] object-cover"
+                    width={260}
+                    height={260}
+                    alt="Curved Text"
+                    src="/curvetext.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* ABOUT US*/}
+        <section
+          id="about-us"
+          className="w-full relative bg-background overflow-y-auto flex flex-row gap-5 items-start justify-center py-[60px] px-10 box-border text-left text-2xl text-black font-3 md:pt-5 pb-10 md:px-20"
+        >
+          <div className="">
+            <div className="">
+              <h1 className=""> </h1>
+              <Image className="" width={128} height={128} alt="" src="/about-arrow.png" />
+            </div>
+            <div className="">
+              <h1 className=""></h1>
+              <Image className="" width={932} height={154} alt="" src="/aboutimg4.png" />
+            </div>
+          </div>
+
+          <div className="">
+            <div className="">
+              <p className=""></p>
+              <p className=""></p>
+            </div>
+            <Image className="" width={594} height={245} alt="" src="/aboutimg1.png" />
+          </div>
+
+          <div className="">
+            <Image className="" width={523} height={174} alt="" src="/aboutimg2.png" />
+            <div className="">
+              <p className=""></p>
+              <p className=""></p>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="">
+              <h1 className=""></h1>
+              <p className=""></p>
+            </div>
+            <Image className="" width={549} height={154} alt="" src="/aboutimg3.png" />
+          </div>
+        </section>
+
+        {/* OUR SERVICES*/}
+        <section>
+          <div className="">
+            <h1 className=""></h1>
+            <h1 className=""></h1>
+          </div>
+          <div className="">
+            <div className="">
+              <p className=""></p>
+              <Image className="" width={621} height={387} alt="" src="/servicesimg.png" />
+            </div>
+            <div className="">
+              <div className="">
+                <h2 className=""></h2>
+                <p className=""></p>
+              </div>
+              <div className="">
+                <h2 className=""></h2>
+                <p className=""></p>
+              </div>
+              <div className="">
+                <h2 className=""></h2>
+                <p className=""></p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* OUR PROJECTS*/}
+        <section>
+          <div className="">
+            <h1 className=""></h1>
+            <h1 className=""></h1>
+          </div>
+          {/*Project Summary component*/}
+        </section>
+
+        {/* TESTIMONIALS */}
+        <section>
+          <div className="">
+            {/* Carousel component */}
+            <div className=""></div>
+            <div className=""></div>
+          </div>
+          <div className="">
+            <div className="">
+              <h1 className=""></h1>
+              <p className=""></p>
+            </div>
+            <div className="">{/* Testimonials component */}</div>
+          </div>
+        </section>
+
+        {/* CONTACT US*/}
+        <section>
+          <div className="">
+            <div className="">
+              <div className="">
+                <h1 className=""></h1>
+                <p className=""></p>
+              </div>
+              <div className="">
+                <p className=""></p>
+                <p className=""></p>
+              </div>
+            </div>
+            <div className="">
+              <div className="">
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+              </div>
+              <div className="">
+                <div className=""></div>
+              </div>
+              <div className="">
+                <div className=""></div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer>
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </footer>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
